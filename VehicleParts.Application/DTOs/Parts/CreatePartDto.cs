6f5b8900e317af523607d44beb0d5761ea91f246
aseem-gautam <gautamaseem3@gmail.com>
2026-05-19 10:36:39 +0545

    Creation of Parts DTO

---
+++ VehicleParts.Application/DTOs/Parts/CreatePartDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleParts.Application.DTOs.Parts;
+
+public class CreatePartDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public string Category { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+    public int MinimumStockLevel { get; set; }
+    public int VendorId { get; set; }
+}
